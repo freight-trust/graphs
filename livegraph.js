4b02ea97f3f2493ea61c20ecb47da9b884d3df62
sam <sam@freighttrust.com>
2020-07-30 03:17:18 -0700

    feat(release): v0.2.0
    release version 0.2.0 of simulator

--- livegraph.js
+++ livegraph.js
@@ -16,122 +16,133 @@
 "use strict";
 
 function liveGraph(targetDiv, data, parameters) {
+  var canvasHeight = 500;
+  var canvasWidth = 500;
+  var padding = 50;
+  var plotType = "linear";
+  var caption = null;
 
-    var canvasHeight = 500;
-    var canvasWidth = 500;
-    var padding = 50;
-    var plotType = 'linear';
-    var caption = null;
-
-    if (parameters) {
-        if (parameters.plotType) {
-            plotType = parameters.plotType;
-        }
-        caption = parameters.caption;
+  if (parameters) {
+    if (parameters.plotType) {
+      plotType = parameters.plotType;
     }
-    this.svg = d3.select(targetDiv).append("svg")
-                .attr("width", canvasWidth)
-                .attr("height", canvasHeight);
+    caption = parameters.caption;
+  }
+  this.svg = d3
+    .select(targetDiv)
+    .append("svg")
+    .attr("width", canvasWidth)
+    .attr("height", canvasHeight);
 
-    this.svg.append("text")
-        .attr("x", (canvasWidth / 2))
-        .attr("y", 20)
-        .attr("text-anchor", "middle")
-        .style("font-size", "16px")
-        .text(caption);
+  this.svg
+    .append("text")
+    .attr("x", canvasWidth / 2)
+    .attr("y", 20)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .text(caption);
 
-    var xScale = d3.scale.linear()
-                    .domain([0, 100])
-                    .range([padding, canvasWidth - padding * 2]);
+  var xScale = d3.scale
+    .linear()
+    .domain([0, 100])
+    .range([padding, canvasWidth - padding * 2]);
 
-    var yScale;
-    if (plotType === 'semilog') {
-        yScale = d3.scale.log()
-                    .domain([0, 500])
-                    .range([canvasHeight - padding, padding]);
-    } else if (plotType === 'linear') {
-        yScale = d3.scale.linear()
-                    .domain([0, 500])
-                    .range([canvasHeight - padding, padding]);
-    } else {
-        throw "Unknown scale type " + plotType;
+  var yScale;
+  if (plotType === "semilog") {
+    yScale = d3.scale
+      .log()
+      .domain([0, 500])
+      .range([canvasHeight - padding, padding]);
+  } else if (plotType === "linear") {
+    yScale = d3.scale
+      .linear()
+      .domain([0, 500])
+      .range([canvasHeight - padding, padding]);
+  } else {
+    throw "Unknown scale type " + plotType;
+  }
+
+  var xAxis = d3.svg
+    .axis()
+    .scale(xScale)
+    .orient("bottom")
+    .tickValues(d3.range(0, 100, 25));
+
+  var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
+
+  this.svg
+    .append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + (canvasHeight - padding) + ")")
+    .call(xAxis);
+
+  this.svg
+    .append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
+  /*
+   * Member function to update the graph when new data ticks in
+   */
+  this.update = function (data, xVar, yVar) {
+    xScale.domain([
+      0,
+      d3.max(data, function (d) {
+        return d[xVar];
+      }),
+    ]);
+    yScale.domain([
+      d3.min(data, function (d) {
+        return d[yVar];
+      }),
+      d3.max(data, function (d) {
+        return d[yVar];
+      }),
+    ]);
+    this.svg.select(".x.axis").call(xAxis);
+
+    this.svg.select(".y.axis").call(yAxis);
+
+    //Don't show tick labels if there are too many of them
+    if (yScale.ticks().length > 150) {
+      this.svg.select(".y.axis").call(yAxis).selectAll("text").remove();
     }
 
-    var xAxis = d3.svg.axis()
-                    .scale(xScale)
-                    .orient("bottom")
-                    .tickValues(d3.range(0, 100, 25));;
+    var circle = this.svg.selectAll("circle").data(data, function key(d) {
+      return d[xVar];
+    });
 
-    var yAxis = d3.svg.axis()
-                    .scale(yScale)
-                    .orient("left")
-                    .ticks(5);
+    circle
+      .attr("cx", function (d) {
+        return xScale(d[xVar]);
+      })
+      .attr("cy", function (d) {
+        return yScale(d[yVar]);
+      });
 
-    this.svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + (canvasHeight - padding) +")")
-        .call(xAxis);
+    circle
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) {
+        return xScale(d[xVar]);
+      })
+      .attr("cy", function (d) {
+        return yScale(d[yVar]);
+      })
+      .transition()
+      .duration(1000)
+      .each("start", function () {
+        d3.select(this).attr("class", "datadotsmall").attr("r", 2);
+      })
+      .each("end", function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr("class", "datadotlarge")
+          .attr("r", 5);
+      });
 
-    this.svg.append("g")
-        .attr("class", "y axis")
-        .attr("transform", "translate(" + padding +",0)")
-        .call(yAxis);
-
-    /*
-     * Member function to update the graph when new data ticks in
-     */
-    this.update = function(data, xVar, yVar) {
-
-        xScale.domain([0, d3.max(data, function(d) {
-            return d[xVar]; })]);
-        yScale.domain([d3.min(data, function(d) {
-            return d[yVar]; }),
-                      d3.max(data, function(d) {
-            return d[yVar]; })]);
-        this.svg.select(".x.axis")
-            .call(xAxis);
-
-        this.svg.select(".y.axis")
-            .call(yAxis);
-
-        //Don't show tick labels if there are too many of them
-        if(yScale.ticks().length > 150) {
-            this.svg.select(".y.axis")
-                    .call(yAxis).selectAll("text").remove();
-        };
-
-        var circle = this.svg.selectAll("circle").data(data, function key(d) {return d[xVar];});
-
-        circle.attr("cx", function(d) {
-                return xScale(d[xVar]);
-            })
-            .attr("cy", function(d) {
-                return yScale(d[yVar]);
-            })
-
-        circle.enter()
-            .append("circle")
-            .attr("cx", function(d) {
-                return xScale(d[xVar]);
-            })
-            .attr("cy", function(d) {
-                return yScale(d[yVar]);
-            })
-        .transition()
-        .duration(1000)
-        .each("start", function() {
-            d3.select(this)
-                .attr("class", "datadotsmall")
-                .attr("r", 2);
-        })
-        .each("end", function() {
-            d3.select(this)
-                .transition()
-                .duration(500)
-                .attr("class", "datadotlarge")
-                .attr("r", 5);
-        });
-
-        circle.exit().remove();
-    }
+    circle.exit().remove();
+  };
 }
--- packages/default.js
+++ packages/default.js
@@ -1,147 +1,147 @@
 module.exports = {
-    links: [
-        {
-            source: "Staking Pool",
-            target: "Besu Redis",
-        },
-        {
-            source: "Staking Pool",
-            target: "Event Stream Layer",
-        },
-        {
-            source: "Staking Pool",
-            target: "Authority Node",
-        },
-        {
-            source: "Staking Pool",
-            target: "Kafka/MQ",
-        },
-        {
-            source: "Staking Pool",
-            target: "Fail Over Node",
-        },
-        {
-            source: "Staking Pool",
-            target: "AUX",
-        },
-        {
-            source: "Besu Redis",
-            target: "Authority Node",
-        },
-        {
-            source: "Besu Redis",
-            target: "Kafka/MQ",
-        },
-        {
-            source: "Besu Redis",
-            target: "Fail Over Node",
-        },
-        {
-            source: "Besu Redis",
-            target: "AUX",
-        },
-        {
-            source: "Event Stream Layer",
-            target: "Kafka/MQ",
-        },
-        {
-            source: "Event Stream Layer",
-            target: "Fail Over Node",
-        },
-        {
-            source: "Event Stream Layer",
-            target: "AUX",
-        },
-        {
-            source: "Authority Node",
-            target: "Fail Over Node",
-        },
-        {
-            source: "Authority Node",
-            target: "AUX",
-        },
-        {
-            source: "Fail Over Node",
-            target: "AUX",
-        },
-        {
-            source: "Staking Pool",
-            target: "Guardian",
-        },
-        {
-            source: "Besu Redis",
-            target: "AS2 Protocol",
-        },
-        {
-            source: "Event Stream Layer",
-            target: "Persistance Layer",
-        },
-        {
-            source: "Authority Node",
-            target: "Kube Pod",
-        },
-        {
-            source: "Kafka/MQ",
-            target: "Maidenlane",
-        },
-        {
-            source: "Fail Over Node",
-            target: "Message Broker",
-        },
-        {
-            source: "AUX",
-            target: "Ethereum Mainnet",
-        },
-    ],
-    nodes: [
-        {
-            id: "Staking Pool",
-        },
-        {
-            id: "Besu Redis",
-        },
-        {
-            id: "Event Stream Layer",
-        },
-        {
-            id: "Authority Node",
-        },
-        {
-            id: "Kafka/MQ",
-        },
-        {
-            id: "Fail Over Node",
-        },
-        {
-            id: "AUX",
-        },
-        {
-            id: "Guardian",
-            symbolType: "square",
-        },
-        {
-            id: "AS2 Protocol",
-            symbolType: "square",
-        },
-        {
-            id: "Persistance Layer",
-            symbolType: "square",
-        },
-        {
-            id: "Kube Pod",
-            symbolType: "square",
-        },
-        {
-            id: "Maidenlane",
-            symbolType: "square",
-        },
-        {
-            id: "Message Broker",
-            symbolType: "square",
-        },
-        {
-            id: "Ethereum Mainnet",
-            symbolType: "square",
-        },
-    ],
+  links: [
+    {
+      source: "Staking Pool",
+      target: "Besu Redis",
+    },
+    {
+      source: "Staking Pool",
+      target: "Event Stream Layer",
+    },
+    {
+      source: "Staking Pool",
+      target: "Authority Node",
+    },
+    {
+      source: "Staking Pool",
+      target: "Kafka/MQ",
+    },
+    {
+      source: "Staking Pool",
+      target: "Fail Over Node",
+    },
+    {
+      source: "Staking Pool",
+      target: "AUX",
+    },
+    {
+      source: "Besu Redis",
+      target: "Authority Node",
+    },
+    {
+      source: "Besu Redis",
+      target: "Kafka/MQ",
+    },
+    {
+      source: "Besu Redis",
+      target: "Fail Over Node",
+    },
+    {
+      source: "Besu Redis",
+      target: "AUX",
+    },
+    {
+      source: "Event Stream Layer",
+      target: "Kafka/MQ",
+    },
+    {
+      source: "Event Stream Layer",
+      target: "Fail Over Node",
+    },
+    {
+      source: "Event Stream Layer",
+      target: "AUX",
+    },
+    {
+      source: "Authority Node",
+      target: "Fail Over Node",
+    },
+    {
+      source: "Authority Node",
+      target: "AUX",
+    },
+    {
+      source: "Fail Over Node",
+      target: "AUX",
+    },
+    {
+      source: "Staking Pool",
+      target: "Guardian",
+    },
+    {
+      source: "Besu Redis",
+      target: "AS2 Protocol",
+    },
+    {
+      source: "Event Stream Layer",
+      target: "Persistance Layer",
+    },
+    {
+      source: "Authority Node",
+      target: "Kube Pod",
+    },
+    {
+      source: "Kafka/MQ",
+      target: "Maidenlane",
+    },
+    {
+      source: "Fail Over Node",
+      target: "Message Broker",
+    },
+    {
+      source: "AUX",
+      target: "Ethereum Mainnet",
+    },
+  ],
+  nodes: [
+    {
+      id: "Staking Pool",
+    },
+    {
+      id: "Besu Redis",
+    },
+    {
+      id: "Event Stream Layer",
+    },
+    {
+      id: "Authority Node",
+    },
+    {
+      id: "Kafka/MQ",
+    },
+    {
+      id: "Fail Over Node",
+    },
+    {
+      id: "AUX",
+    },
+    {
+      id: "Guardian",
+      symbolType: "square",
+    },
+    {
+      id: "AS2 Protocol",
+      symbolType: "square",
+    },
+    {
+      id: "Persistance Layer",
+      symbolType: "square",
+    },
+    {
+      id: "Kube Pod",
+      symbolType: "square",
+    },
+    {
+      id: "Maidenlane",
+      symbolType: "square",
+    },
+    {
+      id: "Message Broker",
+      symbolType: "square",
+    },
+    {
+      id: "Ethereum Mainnet",
+      symbolType: "square",
+    },
+  ],
 };
